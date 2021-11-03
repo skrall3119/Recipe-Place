548238b4c98d97bff96a6d781dc0ad2871742284
skrall3119 <alexjanci@yahoo.com>
2021-11-03 11:46:55 -0500

    created initial layout, navigation functionality

---
+++ Recipe Place/InstructionsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InstructionsViewController.swift
+//  Recipe Place
+//
+//  Created by Alex Janci on 11/3/21.
+//
+
+import UIKit
+
+class InstructionsViewController: UIViewController{
+    
+    @IBAction func saveRecipe(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
